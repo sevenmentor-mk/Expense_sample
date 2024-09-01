193b142f4ab91db60517c922c8d056d1000a098e
sevenmentor-mk <mkankrale@sevenmentor.com>
2024-09-01 14:37:08 +0530

    Delete Expense_tracker directory

--- Expense_tracker/ex.py
+++
@@ -1,8 +0,0 @@
-class expense:
-    def __init__(self,name,amt,cat):
-        self.name = name
-        self.amount = amt
-        self.category = cat
-    
-    def __repr__(self):
-        return f'{self.name},{self.amount:.2f},{self.category}'
